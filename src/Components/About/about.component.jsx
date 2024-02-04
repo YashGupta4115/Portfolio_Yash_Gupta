d57080c7c963b2c006f3d71781e6938d0d2b2566
Yash Gupta <yashguptayashu2003@gmail.com>
2024-02-04 11:40:15 +0530

    About and Contact page build

--- src/Components/About/about.component.jsx
+++ src/Components/About/about.component.jsx
@@ -1,12 +1,23 @@
 import Navigation from '../navigationBar/navigation.component';
 import './about.styles.scss';
+import profile from '../../assets/profile.png';
 
 const About = () => {
     return(
         <div id="about"className='about-container'>
             <Navigation/>
             <div className='about-items-container'>
-                
+                <div className='about-items-heading'>ABOUT ME</div>
+                <div className='about-items-components'>
+                    <div className='about-items-container-left'>
+                        <img className='profile_picture' alt='profile-pic' src={profile}/>
+                    </div>
+                    <div className='about-items-container-right'>
+                        <div className='about-right-container-Description'>
+                        Hi, My name is Yash Gupta. I am a highly motivated and detail-orientedcomputer science student at KIIT, graduating in 2025. My courseworkhas provided me with a strong foundation in machine learning, datamining, and artificial intelligence. Eager to contribute to innovativeteams, I am passionate about leveraging my skills and knowledge indata science to tackle real-world problems.
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/Components/Contact/contact.component.jsx
+++ src/Components/Contact/contact.component.jsx
@@ -1,22 +1,32 @@
 import Navigation from '../navigationBar/navigation.component';
 import './contact.styles.scss';
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
+import { FaPhoneAlt } from "react-icons/fa";
 
 const ContactMe = ()=>{
     return (
         <div id="contact"className='contact-container'>
             <Navigation/>
             <div className='contact-items-container'>
-                <div className='phone-contact general-container'>
-                    <div className='contact-container-heading'>Phone</div>
-                    <div className='contact-container-desciption'>6201461718</div>
-                </div>
-                <div className='email-contact general-container'>
-                    <div className='contact-container-heading'>E-mail</div>
-                    <div className='contact-container-desciption'>yashguptayashu2003@gmail.com</div>
-                </div>
-                <div className='address-contact general-container'>
-                    <div className='contact-container-heading'>Address</div>
-                    <div className='contact-container-desciption'>KIIT University</div>
+                <div className='contact-item-heading'>Contact Me</div>
+                <div className='contact-items-components'>
+                    <div className='contact-left-component'>
+                        <span className='email-contact'><IoIosMail size={30}  className='icons'/>yashguptayashu2003@gmail.com</span>
+                        <span className='phone-contact'><FaPhoneAlt size={30}  className='icons'/>6201461718</span>
+                        <div className='social-icons-container'>
+                            <a className='icon-links' href='https://github.com/YashGupta4115' target='_blank' rel="noreferrer"><FaGithub  size={30} className='icons'/></a>
+                            <a className='icon-links' href='https://www.linkedin.com/in/yash-gupta-489922260/'  target='_blank' rel="noreferrer"><FaLinkedin size={30} className='icons'/></a>
+                        </div>
+                        <button className='contact-button'>Download Resume</button>
+                    </div>
+                    <div className='contact-right-component'>
+                        <input placeholder='Your Name'type='text' className='contact-right-input'/>
+                        <input type='email' placeholder='Your Email' className='contact-right-input'/>
+                        <textarea placeholder='Your Message'className='contact-right-textarea'rows={8} cols={48}/>
+                        <button className='contact-button'>Submit</button>
+                    </div>
                 </div>
             </div>     
         </div>
