4fc73056488410067e28a926e530aa3ac87bac82
Yash Gupta <yashguptayashu2003@gmail.com>
2024-06-08 10:48:40 +0530

    added new project ( form and form validation )

--- src/Components/Projects/projects.component.jsx
+++ src/Components/Projects/projects.component.jsx
@@ -6,97 +6,107 @@ import portfolio_diplay from '../../assets/Screenshot 2024-01-31 160235.png';
 import rentalProject_display from '../../assets/Rental Project SS.png';
 import mlProjects_display from '../../assets/Predictive analysis.png';
 import magic_cards_display from '../../assets/magic_card.png';
+import form_and_form_validation from '../../assets/formAndformValidation.png';
 import './projects.styles.scss';
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 
 const projects = [
     {
-        projectDisplay : vogue_variety_display,
-        projectTitle : 'Vouge Variety',
-        projectDesp : 'E-Commerce Website Based on React',
-        route : 'https://voguevariety.netlify.app/',
-        id : '1',
-        initial : -1000,
-        duration : 0.3,
+        projectDisplay: vogue_variety_display,
+        projectTitle: 'Vouge Variety',
+        projectDesp: 'E-Commerce Website Based on React',
+        route: 'https://voguevariety.netlify.app/',
+        id: '1',
+        initial: -1000,
+        duration: 0.3,
     },
     {
-        projectDisplay : youtube_clone_display,
-        projectTitle : 'Youtube Clone',
-        projectDesp : 'YouTube Clone based on React',
-        route : 'https://youtubeclonebyyashgupta.netlify.app/',
-        id : '2',
-        initial : 1000,
-        duration : 0.4,
+        projectDisplay: youtube_clone_display,
+        projectTitle: 'Youtube Clone',
+        projectDesp: 'YouTube Clone based on React',
+        route: 'https://youtubeclonebyyashgupta.netlify.app/',
+        id: '2',
+        initial: 1000,
+        duration: 0.4,
     },
     {
-        projectDisplay : portfolio_diplay,
-        projectTitle : 'Porfolio',
-        projectDesp : 'A Portfolio Websote Based on React',
-        route : 'https://portfolio-yashgupta.netlify.app/',
-        id : '3',
-        initial : 1000,
-        duration : 0.4,
+        projectDisplay: portfolio_diplay,
+        projectTitle: 'Porfolio',
+        projectDesp: 'A Portfolio Websote Based on React',
+        route: 'https://portfolio-yashgupta.netlify.app/',
+        id: '3',
+        initial: 1000,
+        duration: 0.4,
     },
     {
-        projectDisplay : rentalProject_display,
-        projectTitle : 'Product Rental Application',
-        projectDesp : 'Applicaion based in JAVA GUI and DBMS',
-        route : 'https://github.com/YashGupta4115/Product_Rental_System/tree/master',
-        id : '4',
-        initial : -1000,
-        duration : 0.6,
+        projectDisplay: rentalProject_display,
+        projectTitle: 'Product Rental Application',
+        projectDesp: 'Applicaion based in JAVA GUI and DBMS',
+        route: 'https://github.com/YashGupta4115/Product_Rental_System/tree/master',
+        id: '4',
+        initial: -1000,
+        duration: 0.6,
     },
     {
-        projectDisplay : mlProjects_display,
-        projectTitle : 'Predictive Analysis',
-        projectDesp : 'Multiple ML Projects',
-        route : 'https://github.com/YashGupta4115/Machine-Learning-and-Data-Science-Projects',
-        id : '5',
-        initial : -1000,
-        duration : 0.3,
+        projectDisplay: mlProjects_display,
+        projectTitle: 'Predictive Analysis',
+        projectDesp: 'Multiple ML Projects',
+        route: 'https://github.com/YashGupta4115/Machine-Learning-and-Data-Science-Projects',
+        id: '5',
+        initial: -1000,
+        duration: 0.3,
     },
     {
-        projectDisplay : magic_cards_display,
-        projectTitle : 'Magic Card Game',
-        projectDesp : 'React Based guess game',
-        route : '',
-        id : '6',
-        initial : -1000,
-        duration : 0.6,
+        projectDisplay: magic_cards_display,
+        projectTitle: 'Magic Card Game',
+        projectDesp: 'React Based guess game',
+        route: '',
+        id: '6',
+        initial: -1000,
+        duration: 0.6,
+    },
+    {
+        projectDisplay: form_and_form_validation,
+        projectTitle: 'Form & Form Validation',
+        projectDesp: 'React Based Sign Up page',
+        id: '7',
+        initial: 800,
+        duration: 0.4,
+        route: 'https://csi-formandformvalidation-yashgupta.netlify.app/'
     }
 ]
 
 const Projects = () => {
     return (
         <div className="projects-container">
-            <Navigation/>
+            <Navigation />
             <div className='project-items-container'>
-                <motion.div 
+                <motion.div
                     className='project-items-heading'
                     initial={{ y: -100 }}
-                        animate={{ y:0 }}
-                        transition={{
-                            duration: 0.5,
-                            ease: [0, 1, 0.3, 0.4],
-                        }}
+                    animate={{ y: 0 }}
+                    transition={{
+                        duration: 0.5,
+                        ease: [0, 1, 0.3, 0.4],
+                    }}
                 >
                     Projects
                 </motion.div>
                 <div className='projects-inside-container'>
                     {
-                        projects.map((item)=> {
+                        projects.map((item) => {
                             return (
                                 <Link className='project-items-link' key={item.id} to={item.route} target='_blank'>
-                                    <motion.div 
+                                    <motion.div
                                         className='project-items'
-                                        initial={{x : item.initial}}
-                                        animate={{x : 0}}
+                                        initial={{ x: item.initial }}
+                                        animate={{ x: 0 }}
                                         transition={{
-                                            x : {duration : item.duration,ease: [0, 1, 0.3, 0.4]}
-                                        }}    
+                                            x: { duration: item.duration, ease: [0, 1, 0.3, 0.4] }
+                                        }}
                                     >
                                         <span className='projectImage-container'>
-                                            <img className="projectImage" alt={item.id} src={item.projectDisplay}/>
+                                            <img className="projectImage" alt={item.id} src={item.projectDisplay} />
                                         </span>
                                         <span className='projectTitle'>{item.projectTitle}</span>
                                         <span className='projectDesp'>{item.projectDesp}</span>
